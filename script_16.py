3970099c4bc6fd9bcdc73d479f45eaeac47663dc
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:55:27 +0530

    Upload script_16.py

---
+++ script_16.py
@@ -0,0 +1,12 @@
+from main.tools import template
+import os
+def main():
+    print("\n[+] FrameWork")
+    os.system("clear")
+    template.template("Framework","no-tools","Writeups",
+                    {
+                            "OSINT Framework":"https://osintframework.com/",
+                            "MITRE Framework":"https://attack.mitre.org/",
+                    })
+if __name__=='__main__':
+    main()
