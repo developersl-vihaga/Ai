8161db338f23d728c3b38ce54998260179542de7
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:48:00 +0530

    Upload script_685.py

---
+++ script_685.py
@@ -0,0 +1,20 @@
+import pytest
+import logging
+import os
+from deathstar.empire import EmpireApiClient, EmpireModuleExecutionTimeout
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("[%(name)s] %(levelname)s - %(message)s"))
+log = logging.getLogger("deathstar")
+log.setLevel(logging.DEBUG)
+log.addHandler(handler)
+@pytest.mark.asyncio
+@pytest.fixture
+async def empire():
+    empire = EmpireApiClient(host=os.environ["EMPIRE_HOST"])
+    await empire.login("empireadmin", "Password123!")
+    yield empire
+@pytest.mark.asyncio
+@pytest.fixture
+async def agents(empire):
+    agents = await empire.agents.get()
+    yield agents
