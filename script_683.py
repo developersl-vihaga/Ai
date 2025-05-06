a6748b00050621d2e4095706ea3c172ab5d29899
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 02:01:27 +0530

    Upload script_683.py

---
+++ script_683.py
@@ -0,0 +1,22 @@
+import pytest
+import logging
+import asyncio
+from deathstar.kybercrystals import KyberCrystals
+class MockDeathStar:
+    pass
+@pytest.fixture
+def kyber_crystals(empire):
+    dt = MockDeathStar()
+    dt.empire = empire
+    k = KyberCrystals(dt)
+    return k
+@pytest.mark.asyncio
+async def test_kyber_crystallization(kyber_crystals):
+    assert kyber_crystals.get_domain_sid
+    for crystal in kyber_crystals.loaded:
+        assert isinstance(crystal.log, logging.Logger)
+        assert isinstance(crystal.deathstar, MockDeathStar)
+@pytest.mark.asyncio
+async def test_crystal_focus(kyber_crystals, agents):
+    for agent in agents:
+        await kyber_crystals.tasklist(agent)
