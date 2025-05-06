06db4682bba626a3ba728fadc1567a77d0ab67dc
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 02:00:51 +0530

    Upload script_291.py

---
+++ script_291.py
@@ -0,0 +1,69 @@
+class Module:
+    def __init__(self, mainMenu, params=[]):
+        self.info = {
+            'Name': 'Get User Information',
+            'Author': ['@424f424f'],
+            'Description': 'This module will return the user profile specified',
+            'Background' : False,
+            'OutputExtension' : "",
+            'NeedsAdmin' : False,
+            'OpsecSafe' : True,
+            'Language' : 'python',
+            'MinLanguageVersion' : '2.6',
+            'Comments': ['']
+        }
+        self.options = {
+            'Agent' : {
+                'Description'   :   'Agent to run on.',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'LDAPAddress' : {
+                'Description'   :   'LDAP IP/Hostname',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'BindDN' : {
+                'Description'   :   'user@penlab.local',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'Password' : {
+                'Description'   :   'Password to connect to LDAP',
+                'Required'      :   False,
+                'Value'         :   ''
+            },
+            'user' : {
+                'Description'   :   'User to check group memberships of',
+                'Required'      :   False,
+                'Value'         :   ''
+            }
+        }
+        self.mainMenu = mainMenu
+        if params:
+            for param in params:
+                option, value = param
+                if option in self.options:
+                    self.options[option]['Value'] = value
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+        LDAPAddress = self.options['LDAPAddress']['Value']
+        BindDN = self.options['BindDN']['Value']
+        password = self.options['Password']['Value']
+        user = self.options['user']['Value']
+        script = """
+import sys, os, subprocess, re
+BindDN = "%s"
+LDAPAddress = "%s"
+password = "%s"
+user = "%s"
+regex = re.compile('.+@([^.]+)\..+')
+global tld
+match = re.match(regex, BindDN)
+tld = match.group(1)
+global ext
+ext = BindDN.split('.')[1]
+cmd = \"""ldapsearch -x -h {} -b "dc={},dc={}" -D {} -w {} "(samAccountName="{}")" ""\".format(LDAPAddress, tld, ext, BindDN, password, user)
+print ""
+print subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+""" % (BindDN, LDAPAddress, password, user)
+        return script
