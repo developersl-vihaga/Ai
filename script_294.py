1187ab1b6d299cb9b23a045d1d792edfe4faba2b
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:58:52 +0530

    Upload script_294.py

---
+++ script_294.py
@@ -0,0 +1,73 @@
+class Module:
+    def __init__(self, mainMenu, params=[]):
+        self.info = {
+            'Name': 'Get Group Members',
+            'Author': ['@424f424f'],
+            'Description': 'This module will return a list of group members',
+            'Background' : False,
+            'OutputExtension' : "",
+            'NeedsAdmin' : False,
+            'OpsecSafe' : True,
+            'Language' : 'python',
+            'MinLanguageVersion' : '2.6',
+            'Comments': ['']
+        }
+        self.options = {
+            'Agent' : {
+                'Description'   :   'Agent to run on.',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'LDAPAddress' : {
+                'Description'   :   'LDAP IP/Hostname',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'BindDN' : {
+                'Description'   :   'user@penlab.local',
+                'Required'      :   True,
+                'Value'         :   ''
+            },
+            'Password' : {
+                'Description'   :   'Password to connect to LDAP',
+                'Required'      :   False,
+                'Value'         :   ''
+            },
+            'groupname' : {
+                'Description'   :   'Group to check which users are a member of',
+                'Required'      :   False,
+                'Value'         :   'Domain Admins'
+            }
+        }
+        self.mainMenu = mainMenu
+        if params:
+            for param in params:
+                option, value = param
+                if option in self.options:
+                    self.options[option]['Value'] = value
+    def generate(self, obfuscate=False, obfuscationCommand=""):
+        LDAPAddress = self.options['LDAPAddress']['Value']
+        BindDN = self.options['BindDN']['Value']
+        password = self.options['Password']['Value']
+        groupname = self.options['groupname']['Value']
+        script = """
+import sys, os, subprocess, re
+BindDN = "%s"
+LDAPAddress = "%s"
+password = "%s"
+groupname = "%s"
+regex = re.compile('.+@([^.]+)\..+')
+global tld
+match = re.match(regex, BindDN)
+tld = match.group(1)
+global ext
+ext = BindDN.split('.')[1]
+cmd = \"""ldapsearch -x -h {} -b "dc={},dc={}" -D {} -w {} "cn={}" ""\".format(LDAPAddress, tld, ext, BindDN, password, groupname)
+output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+output2 = subprocess.Popen(["grep", "member:"],stdin=output.stdout, stdout=subprocess.PIPE,universal_newlines=True)
+output.stdout.close()
+out,err = output2.communicate()
+print ""
+print out
+""" % (BindDN, LDAPAddress, password, groupname)
+        return script
