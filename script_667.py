6d14fa8304d401b20c41c5cc0e51b70dc91f8e8a
developersl-vihaga <githubdevelopersl@gmail.com>
2025-05-07 01:58:08 +0530

    Upload script_667.py

---
+++ script_667.py
@@ -0,0 +1,5 @@
+async def crystallize(agent):
+    output = await agent.execute("powershell/management/get_domain_sid")
+    domain_sid = output["results"].splitlines()[0]
+    log.debug(domain_sid)
+    return domain_sid
